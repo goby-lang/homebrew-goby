cbb108405aaaeeabfc551ab2d208af5eea38a50f
Stan Lo <a22301613@yahoo.com.tw>
2017-05-07 19:13:54 +0800

    Update sha again.

--- Formula/rooby.rb
+++ Formula/rooby.rb
@@ -2,8 +2,7 @@ class Rooby < Formula
   desc "A new object oriented language written in Go aim at developing microservice efficiently."
   homepage "https://rooby-lang.github.io/rooby"
   url "https://github.com/rooby-lang/rooby/archive/0.0.1.tar.gz"
-  version "0.0.1"
-  sha256 "dee9bf61fb9740dc18e0200a1cce93fcce98f3ac0cd85fc6c924dcdca961fa6e"
+  sha256 "672a9eeb355743c51ca512f5348f0d01359f3820fc7d2977e1a9cddb799bd735"
 
   def install
     bin.install "bin/rooby"
