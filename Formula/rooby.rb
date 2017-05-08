f7b9d31b895479f566f49739545a9b114b185f58
Stan Lo <stan001212@gmail.com>
2017-05-08 19:08:54 +0800

    Update sha256

--- Formula/rooby.rb
+++ Formula/rooby.rb
@@ -3,7 +3,7 @@ class Rooby < Formula
   homepage "https://rooby-lang.github.io/rooby"
   url "https://github.com/rooby-lang/rooby/archive/v0.0.1.tar.gz"
   version "0.0.1"
-  sha256 "672a9eeb355743c51ca512f5348f0d01359f3820fc7d2977e1a9cddb799bd735"
+  sha256 "b866dd040fb1e66b2d16a71e954fa35701ed7794f2d6d8151496947678ff5461"
 
   def install
     bin.install "bin/rooby"
