fda59609fae64f61476a1e264d1616149b40f495
Adler <nkj20932@hotmail.com>
2017-05-08 22:13:41 +1000

    update formula to 0.0.1

--- Formula/rooby.rb
+++ Formula/rooby.rb
@@ -5,7 +5,14 @@ class Rooby < Formula
   version "0.0.1"
   sha256 "b866dd040fb1e66b2d16a71e954fa35701ed7794f2d6d8151496947678ff5461"
 
+  resource "rooby" do
+    url "https://github.com/rooby-lang/rooby/releases/download/v0.0.1/rooby"
+    sha256 "7994dfb02b49c12b3c0946dfb2d35ecd8955137022aa59a2c7491f4cc86b1f4a"
+  end
+
   def install
-    bin.install "bin/rooby"
+    resource("rooby").stage do
+      bin.install "rooby"
+    end
   end
 end
