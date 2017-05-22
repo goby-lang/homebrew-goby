2d05e2d6db0738ad84f72d974c5c22937e8764d6
Adler <nkj20932@hotmail.com>
2017-05-22 21:16:12 +1000

    update to v0.0.2

--- Formula/goby.rb
+++ Formula/goby.rb
@@ -1,13 +1,13 @@
 class Goby < Formula
   desc "A new object oriented language written in Go aim at developing microservice efficiently."
   homepage "https://goby-lang.github.io/goby"
-  url "https://github.com/goby-lang/goby/archive/v0.0.1.tar.gz"
-  version "0.0.1"
-  sha256 "c6f8fc204bac2fff1fe9bd61849e147db74517cab3c219b31fab370e40e1db09"
+  url "https://github.com/goby-lang/goby/archive/v0.0.2.tar.gz"
+  version "0.0.2"
+  sha256 "3f45cbd042d2303ad2d0a54f7bc3dd2e2e1ec04867fbf287ddaa5f41c8781271"
 
   resource "goby" do
-    url "https://github.com/goby-lang/goby/releases/download/v0.0.1/goby"
-    sha256 "5825eb9a5d792a6b0918da7e02c2f45dce559aceb80824f9ffd363eb413c6934"
+    url "https://github.com/goby-lang/goby/releases/download/v0.0.2/goby"
+    sha256 "88930ccf70f6429d9e0a1ce898e3cb49d8a056f9b342230d0770719b6fd9f432"
   end
 
   def install
