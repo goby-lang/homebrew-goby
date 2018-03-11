3e53c4cd71436da74201fa9a32d5b8d514c9fdb4
st0012 <stan001212@gmail.com>
2018-03-12 01:14:44 +0800

    Update version for v0.1.8.1

--- Formula/goby.rb
+++ Formula/goby.rb
@@ -1,7 +1,7 @@
 class Goby < Formula
-  VERSION     = "0.1.8"
-  GOBY_SHA256 = "4d01a2abc4da0dcb1de2093dd1e1d4557a7274219b7023cbf794073c7ed3fe03"
-  EXE_SHA256  = "091382a7c4a27ccd99ba044e1cc0ce0cad6be2fff7327dd5375b7b3890b271e7"
+  VERSION     = "0.1.8.1"
+  GOBY_SHA256 = "9487da7b6d1e13f7d40379f8b11b6b79febf5036c9b3bb563cdedfa84e717968"
+  EXE_SHA256  = "4031828e5c839b6aad8d102c0298c77dbab3b212e5eb97c1c7b0628f2091ffb8"
 
   desc "A new object oriented language written in Go aim at developing microservice efficiently."
   homepage "https://goby-lang.github.io/goby"
